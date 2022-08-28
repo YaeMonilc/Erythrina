c4a381e9d647fd881be0ec1ae38743cfff14b323
YaeMonilc <Monilc@outlook.com>
2022-08-28 12:27:17 +0800

    🤤|使上传数据可扩展

--- src/main/java/cc/kocho/data/http/UploadData.java
+++ src/main/java/cc/kocho/data/http/UploadData.java
@@ -1,11 +1,12 @@
 package cc.kocho.data.http;
 
 import cc.kocho.data.image.Tag;
+import dev.morphia.annotations.Entity;
 
 import java.util.ArrayList;
 import java.util.List;
-
-public class Tags {
+@Entity
+public class UploadData {
     private List<Tag> tags = new ArrayList<>();
 
     public List<Tag> getTags() {
